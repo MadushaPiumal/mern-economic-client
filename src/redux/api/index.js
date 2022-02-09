db263fe306cf12266205e8e2b747d2a9b9823142
MadushaPiumal <85735808+MadushaPiumal@users.noreply.github.com>
2022-02-09 22:34:40 +0530

    base url changed

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://127.0.0.1:5000" });
+const API = axios.create({ baseURL: " https://mern-economic.herokuapp.com/" });
 
 // API.interceptors.request.use((req) => {
 //   if (localStorage.getItem("userInfo")) {
